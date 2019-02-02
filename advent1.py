00b0c73ab4c43a1a5ba1e364799c3299e52dd6fb
jmfuch02 <jason.fuchs@gmail.com>
2019-02-02 18:02:26 -0500

    Not adding the frequencies correctly the second time around

--- advent1.py
+++ advent1.py
@@ -6,25 +6,32 @@ Created on Sat Feb  2 17:09:52 2019
 """
 
 freq = 0
-i = 0
 input = []
+freqs = []
 
 # Add up all the frequency shifts
 with open("input.txt") as file:
     for line in file:
+        input.append(tuple((freq, int(line), freq + int(line))))
         freq = freq + int(line)
     print (line, ' ',  freq)
 
-'''        
-with open("input.txt") as file:
-    for line in file:
-        print (i)
-        input.append(tuple((freq, int(line), freq + int(line))))
-        freq = freq + int(line)
-        print (line, ' ',  freq)
-        i = i + 1
 
-'''
-        
+def find_repeat(input_list, freqs):
+    for tuple_item in input_list:
+        if tuple_item[2] in freqs:
+            return tuple_item[2]
+        else:
+            freqs.append(tuple_item[2])
+    return "None"
+
+answer = "None"
+while answer == "None":
+    answer = find_repeat(input, freqs)
+    print (answer)
+
+
+
+    
         
 
