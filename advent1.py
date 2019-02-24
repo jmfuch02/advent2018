63d554d3cb771edcf8dfa0402fc3ce74c708b3fc
jmfuch02 <jason.fuchs@gmail.com>
2019-02-24 16:08:17 -0500

    Added functions to create list from file and apply changes

--- advent1.py
+++ advent1.py
@@ -5,33 +5,57 @@ Created on Sat Feb  2 17:09:52 2019
 @author: Jason
 """
 
-freq = 0
-input = []
-freqs = []
-
 # Add up all the frequency shifts
-with open("input.txt") as file:
-    for line in file:
-        input.append(tuple((freq, int(line), freq + int(line))))
-        freq = freq + int(line)
-    print (line, ' ',  freq)
+def get_freq_1():
+    freq = 0
+    with open("input.txt") as file:
+        for line in file:
+            freq = freq + int(line)
+            print(line, ' ',  freq)
 
 
-def find_repeat(input_list, freqs):
-    for tuple_item in input_list:
-        if tuple_item[2] in freqs:
-            return tuple_item[2]
-        else:
-            freqs.append(tuple_item[2])
-    return "None"
+def find_repeat_2(changes: list):
+    found: bool = False
 
-answer = "None"
-while answer == "None":
-    answer = find_repeat(input, freqs)
-    print (answer)
+    changes = file_to_list()
+
+    while found is False:
+        print('Round #', count)
+        for change in changes:
+            if found is False:
+                freq = freq + int(change)
+                # print(line, ' ', freq)
+                if freq not in freqs:
+                    print(f'Found a new frequency: {freq}')
+                    freqs.append(int(freq))
+                else:
+                    found = True
+                    print('First repeated frequency is', freq)
+                    return freq
+        print(f'Freq at end of round {count} is {freq}')
+        count += 1
 
 
+def apply_changes(changes, frequency_list=[]):
+    freqs = frequency_list
 
-    
-        
+    if not freqs:
+        freqs.append(0)
+        freq = 0
+    else:
+        freq = freqs[-1]
+
+    for change in changes:
+        freq = freq + int(change)
+        freqs.append(freq)
+    return freqs
+
+
+def file_to_list():
+    changes = []
+    with open("input.txt") as file:
+        for line in file:
+            changes.append(int(line))
+    return changes
+
 
