8d38c1893eced0fe82732020b04b3c4dadd08823
jmfuch02 <jason.fuchs@gmail.com>
2019-02-26 22:15:44 -0500

    Seriously all my trouble was caused by not letting it run long enough

--- advent1.py
+++ advent1.py
@@ -5,8 +5,9 @@ Created on Sat Feb  2 17:09:52 2019
 @author: Jason
 """
 
+
 # Add up all the frequency shifts
-def get_freq_1():
+def get_freq():
     freq = 0
     with open("input.txt") as file:
         for line in file:
@@ -14,45 +15,35 @@ def get_freq_1():
             print(line, ' ',  freq)
 
 
-def find_repeat_2(changes: list):
-    found: bool = False
+def loop_device(change_list):
+    seen_freqs = []
+    freq = 0
+    seen_freqs.append(freq)
 
-    changes = file_to_list()
+    while True:
+        for change in change_list:
+            current_frequency = freq
+            resulting_frequency = current_frequency + int(change)
 
-    while found is False:
-        print('Round #', count)
-        for change in changes:
-            if found is False:
-                freq = freq + int(change)
-                # print(line, ' ', freq)
-                if freq not in freqs:
-                    print(f'Found a new frequency: {freq}')
-                    freqs.append(int(freq))
-                else:
-                    found = True
-                    print('First repeated frequency is', freq)
-                    return freq
-        print(f'Freq at end of round {count} is {freq}')
-        count += 1
+            if resulting_frequency not in seen_freqs:
+                # print(f'Current frequency {current_frequency},'
+                #       f'change of {int(change)};'
+                #       f'resulting frequency {resulting_frequency}')
+                seen_freqs.append(resulting_frequency)
+            else:
+                # print(f'Current frequency {current_frequency},'
+                #       f'change of {int(change)};'
+                #       f'resulting frequency {resulting_frequency},'
+                #       f'which has already been seen.')
+                return resulting_frequency
 
-
-def apply_changes(changes, starting_freq=0,  frequency_list=[0]):
-    freqs = frequency_list
-    freq = starting_freq
-
-    for change in changes:
-        freq = freq + int(change)
-        if freq not in freqs:
-            freqs.append(freq)
-        else:
-            print(f'Frequency {freq} is a repeat')
-            return freq, freqs
-    return freq, freqs
+            # Reset for next time
+            freq = resulting_frequency
 
 
-def file_to_list():
+def file_to_list(filename):
     changes = []
-    with open("input.txt") as file:
+    with open(filename) as file:
         for line in file:
             changes.append(int(line))
     return changes
