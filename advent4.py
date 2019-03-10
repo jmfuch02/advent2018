78f28fbdee6099418ba04162047570173888bacb
jmfuch02 <jason.fuchs@gmail.com>
2019-03-10 17:15:15 -0400

    dataframe almost there

--- advent4.py
+++ advent4.py
@@ -11,13 +11,32 @@ times.sort(key=lambda x: x[0])
 print(times)
 
 last_match = ''
-for time in times:
-    match = re.search('#[0-9]*', time[1])
+for event in times:
+    # strip off the year
+    event[0] = (re.sub('[0-9]{4,}-', '', event[0]))
+    # parse time into month, day, hour, minute
+    event[0] = re.split(' |:', event[0])
+    # get the guard id
+    match = re.search('#[0-9]*', event[1])
     if match:
         last_match = match.group(0).replace('#', '')
-        time.append(last_match)
+        event.append(last_match)
     else:
-        time.append(last_match)
+        event.append(last_match)
 
-df = pd.DataFrame(data=times, columns=('date_time', 'note', 'guard_id'))
-df['date_time'] = pd.to_datetime(df['date_time'], yearfirst=True, format='%Y-%m-%d %H:%M')
+fmt = []
+for event in times:
+    print(event)
+    date = event[0][0]
+    minute = event[0][2]
+    desc = event[1]
+    print(f'desc: {desc}')
+    gid = event[2]
+
+    if 'Guard' not in desc:
+        fmt.append([date, gid, desc, minute])
+
+
+df = pd.DataFrame(data=fmt, columns=('date', 'gid', 'desc', 'minute'))
+# df['month_day'] = pd.to_datetime(df['date'], format='%m-%d')
+
